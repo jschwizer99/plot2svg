86d5701459008a335333ec380432df7fc876f614
thomas <thomas_2@wolfdale.(none)>
2015-07-04 20:07:42 +0200

    LatexPassOn option merged.

--- src/plot2svg.m
+++ src/plot2svg.m
@@ -29,7 +29,7 @@ function varargout = plot2svg(param1,id,pixelfiletype)
 %               Clipping
 %               Minor tick marks
 %  22.01.2005 - Removed unused 'end'
-%  29.10.2006 - Bugfix '°','±','µ','²','³','¼''½','¾','©''®'
+%  29.10.2006 - Bugfix '','','','','','''','',''''
 %  17-04-2007 - Bugfix 'projection' in hggroup and hgtransform
 %  27-01-2008 - Added Octave functionality (thanks to Jakob Malm)
 %               Bugfixe cdatamapping (thanks to Tom)
@@ -90,8 +90,8 @@ function varargout = plot2svg(param1,id,pixelfiletype)
 %             - Tiny optimization of the grid display at axis borders
 %  25-08-2011 - Fix for degree character (thanks to Manes Recheis)
 %             - Fix for problems with dash-arrays in Inkscape (thanks to
-%               Rüdiger Stirnberg)
-%             - Modified shape of driangles (thanks to Rüdiger Stirnberg)
+%               Rdiger Stirnberg)
+%             - Modified shape of driangles (thanks to Rdiger Stirnberg)
 %  22-10-2011 - Removed versn as return value of function fileparts (thanks
 %               to Andrew Scott)
 %             - Fix for images (thanks to Roeland)
@@ -146,7 +146,8 @@ function varargout = plot2svg(param1,id,pixelfiletype)
 %  14-05-2015 - Reverted part of the text rendering of 19-02-2015 as it
 %               failed to handle exponents.
 %  14-05-2015 - Removed undefined variable which was obsolete
-
+% Edit by Thomas Wiesner
+%  04-07-2015 - Merge of LatexPassOn option.
 
 
 global PLOT2SVG_globals
@@ -154,6 +155,7 @@ global colorname
 progversion='14-May-2015';
 PLOT2SVG_globals.runningIdNumber = 0;
 PLOT2SVG_globals.octave = false;
+PLOT2SVG_globals.LatexPassOn = false;
 PLOT2SVG_globals.checkUserData = true;
 PLOT2SVG_globals.ScreenPixelsPerInch = 90; % Default 90ppi
 try
@@ -244,6 +246,16 @@ colorname='';
 for i=1:size(cmap,1)
     colorname(i,:)=sprintf('%02x%02x%02x',fix(cmap(i,1)*255),fix(cmap(i,2)*255),fix(cmap(i,3)*255));
 end
+
+if PLOT2SVG_globals.checkUserData && isstruct(get(id,'UserData'))
+    struct_data = get(id,'UserData');
+    if isfield(struct_data,'svg')
+        if isfield(struct_data.svg,'LatexPassOn')
+            PLOT2SVG_globals.LatexPassOn = struct_data.svg.LatexPassOn;
+        end
+    end
+end
+
 
 % Open SVG-file
 [pathstr,name] = fileparts(finalname);
@@ -2611,7 +2623,11 @@ if strcmp(get(ax,'XTickLabelMode'),'auto') && strcmp(get(ax,'XScale'),'linear')
             ratio = 1;
         end
         if round(log10(ratio(1))) ~= 0 && ratio(1) ~= 0
-            exptext = sprintf('&#215; 10<tspan style="font-size:65%%;baseline-shift:super">%g</tspan>', -log10(ratio(1)));
+            if ~PLOT2SVG_globals.LatexPassOn
+                exptext = sprintf('&#215; 10<tspan style="font-size:65%%;baseline-shift:super">%g</tspan>', -log10(ratio(1)));
+            else
+                exptext = sprintf('$\\times 10^{%g}$',-log10(ratio(1)));
+            end
             label2svg(fid,group,axpos,ax,(axpos(1)+axpos(3))*paperpos(3),(1-axpos(2))*paperpos(4)+3*fontsize,exptext,'right',0,'top',1,paperpos,font_color,0)           
         end
     end
@@ -2643,7 +2659,11 @@ if strcmp(get(ax,'YTickLabelMode'),'auto') && strcmp(get(ax,'YScale'),'linear')
             ratio = 1;
         end	
         if round(log10(ratio(1))) ~= 0 && ratio(1) ~= 0
-            exptext = sprintf('&#215; 10<tspan style="font-size:65%%;baseline-shift:super">%g</tspan>', -log10(ratio(1)));
+            if ~PLOT2SVG_globals.LatexPassOn
+                exptext = sprintf('&#215; 10<tspan style="font-size:65%%;baseline-shift:super">%g</tspan>', -log10(ratio(1)));
+            else
+                exptext = sprintf('$\\times 10^{%g}$',-log10(ratio(1)));
+            end
             label2svg(fid,group,axpos,ax,axpos(1)*paperpos(3),(1-(axpos(2)+axpos(4)))*paperpos(4)-0.5*fontsize,exptext,'left',0,'bottom',1,paperpos,font_color,0)           
         end
     end
@@ -2675,7 +2695,11 @@ if strcmp(get(ax,'ZTickLabelMode'),'auto') && strcmp(get(ax,'ZScale'),'linear')
             ratio = 1;
         end
         if round(log10(ratio(1))) ~= 0 && ratio(1) ~= 0
-            exptext = sprintf('&#215; 10<tspan style="font-size:65%%;baseline-shift:super">%g</tspan>', -log10(ratio(1)));
+            if ~PLOT2SVG_globals.LatexPassOn
+                exptext = sprintf('&#215; 10<tspan style="font-size:65%%;baseline-shift:super">%g</tspan>', -log10(ratio(1)));
+            else
+                exptext = sprintf('$\\times 10^{%g}$',-log10(ratio(1)));
+            end
             label2svg(fid,group,axpos,ax,axpos(1)*paperpos(3),(1-(axpos(2)+axpos(4)))*paperpos(4)-0.5*fontsize,exptext,'left',0,'top',1,paperpos,font_color,0)           
         end
     end
@@ -2687,6 +2711,8 @@ end
 % former versions of FrameMaker supported the commands FDY and FDX to shift the text
 % this commands were replaced by a shift parameter that is normed by the font size
 function label2svg(fid,group,axpos,id,x,y,tex,align,angle,valign,lines,paperpos,font_color,exponent)
+global PLOT2SVG_globals
+
 if isempty(tex)
     return;
 end
@@ -2706,6 +2732,9 @@ if isfield(get(id),'Interpreter')
     end
 else
     latex=1;
+end
+if PLOT2SVG_globals.LatexPassOn
+    latex = 0;
 end
 fontsize=convertunit(get(id,'FontSize'),get(id,'FontUnits'),'points', axpos(4));   % convert fontsize to inches
 fontweight = get(id,'FontWeight');
@@ -2866,8 +2895,12 @@ if isempty(tex)
     return;
 end
 if exponent
-    tex=sprintf('10<tspan font-size="%0.1fpt" dy="%0.1fpt">%s</tspan>', 0.7*textfontsize, -0.7*textfontsize, tex);
-    shift = shift + 0.4*fontsize;   % Small correction to make it look nicer
+    if ~PLOT2SVG_globals.LatexPassOn
+        tex = sprintf('10<tspan font-size="%0.1fpt" dy="%0.1fpt">%s</tspan>', 0.7*textfontsize, -0.7*textfontsize, tex);
+        shift = shift + 0.4*fontsize;   % Small correction to make it look nicer
+    else
+        tex = sprintf('$10^{%s}$', tex);
+    end
 end
 % Note: Obviously, Matlab is using font sizes that are rounded to decimal
 % pt sizes. This may cause problems for very small figures. But we have to
@@ -3065,83 +3098,83 @@ if ~isempty(StringText)
     StringText=strrep(StringText,'>','&gt;');
     StringText=strrep(StringText,'"','&quot;');
     % Workaround for Firefox and Inkscape
-    StringText=strrep(StringText,'°','&#176;');
-    %StringText=strrep(StringText,'°','&deg;');
-    StringText=strrep(StringText,'±','&plusmn;');
-    StringText=strrep(StringText,'µ','&micro;');
-    StringText=strrep(StringText,'²','&sup2;');
-    StringText=strrep(StringText,'³','&sup3;');
-    StringText=strrep(StringText,'¼','&frac14;');
-    StringText=strrep(StringText,'½','&frac12;');
-    StringText=strrep(StringText,'¾','&frac34;');
-    StringText=strrep(StringText,'©','&copy;');
-    StringText=strrep(StringText,'®','&reg;');
+    StringText=strrep(StringText,'','&#176;');
+    %StringText=strrep(StringText,'','&deg;');
+    StringText=strrep(StringText,'','&plusmn;');
+    StringText=strrep(StringText,'','&micro;');
+    StringText=strrep(StringText,'','&sup2;');
+    StringText=strrep(StringText,'','&sup3;');
+    StringText=strrep(StringText,'','&frac14;');
+    StringText=strrep(StringText,'','&frac12;');
+    StringText=strrep(StringText,'','&frac34;');
+    StringText=strrep(StringText,'','&copy;');
+    StringText=strrep(StringText,'','&reg;');
     if any(StringText > 190)
-        StringText=strrep(StringText,'¿','&#191;');
-        StringText=strrep(StringText,'À','&#192;');
-        StringText=strrep(StringText,'Á','&#193;');
-        StringText=strrep(StringText,'Â','&#194;');
-        StringText=strrep(StringText,'Ã','&#195;');
-        StringText=strrep(StringText,'Ä','&#196;');
-        StringText=strrep(StringText,'Å','&#197;');
-        StringText=strrep(StringText,'Æ','&#198;');
-        StringText=strrep(StringText,'Ç','&#199;');
-        StringText=strrep(StringText,'È','&#200;');
-        StringText=strrep(StringText,'É','&#201;');
-        StringText=strrep(StringText,'Ê','&#202;');
-        StringText=strrep(StringText,'Ë','&#203;');
-        StringText=strrep(StringText,'Ì','&#204;');
-        StringText=strrep(StringText,'Í','&#205;');
-        StringText=strrep(StringText,'Î','&#206;');
-        StringText=strrep(StringText,'Ï','&#207;');
-        StringText=strrep(StringText,'Ð','&#208;');
-        StringText=strrep(StringText,'Ñ','&#209;');
-        StringText=strrep(StringText,'Ò','&#210;');
-        StringText=strrep(StringText,'Ó','&#211;');
-        StringText=strrep(StringText,'Ô','&#212;');
-        StringText=strrep(StringText,'Õ','&#213;');
-        StringText=strrep(StringText,'Ö','&#214;');
-        StringText=strrep(StringText,'×','&#215;');
-        StringText=strrep(StringText,'Ø','&#216;');
-        StringText=strrep(StringText,'Ù','&#217;');
-        StringText=strrep(StringText,'Ú','&#218;');
-        StringText=strrep(StringText,'Û','&#219;');
-        StringText=strrep(StringText,'Ü','&#220;');
-        StringText=strrep(StringText,'Ý','&#221;');
-        StringText=strrep(StringText,'Þ','&#222;');
-        StringText=strrep(StringText,'ß','&#223;');
-        StringText=strrep(StringText,'à','&#224;');
-        StringText=strrep(StringText,'á','&#225;');
-        StringText=strrep(StringText,'â','&#226;');
-        StringText=strrep(StringText,'ã','&#227;');
-        StringText=strrep(StringText,'ä','&#228;');
-        StringText=strrep(StringText,'å','&#229;');
-        StringText=strrep(StringText,'æ','&#230;');
-        StringText=strrep(StringText,'ç','&#231;');
-        StringText=strrep(StringText,'è','&#232;');
-        StringText=strrep(StringText,'é','&#233;');
-        StringText=strrep(StringText,'ê','&#234;');
-        StringText=strrep(StringText,'ë','&#235;');
-        StringText=strrep(StringText,'ì','&#236;');
-        StringText=strrep(StringText,'í','&#237;');
-        StringText=strrep(StringText,'î','&#238;');
-        StringText=strrep(StringText,'ï','&#239;');
-        StringText=strrep(StringText,'ð','&#240;');
-        StringText=strrep(StringText,'ñ','&#241;');
-        StringText=strrep(StringText,'ò','&#242;');
-        StringText=strrep(StringText,'ó','&#243;');
-        StringText=strrep(StringText,'ô','&#244;');
-        StringText=strrep(StringText,'õ','&#245;');
-        StringText=strrep(StringText,'ö','&#246;');
-        StringText=strrep(StringText,'÷','&#247;');
-        StringText=strrep(StringText,'ø','&#248;');
-        StringText=strrep(StringText,'ù','&#249;');
-        StringText=strrep(StringText,'ú','&#250;');
-        StringText=strrep(StringText,'û','&#251;');
-        StringText=strrep(StringText,'ü','&#252;');
-        StringText=strrep(StringText,'ý','&#253;');
-        StringText=strrep(StringText,'þ','&#254;');
-        StringText=strrep(StringText,'ÿ','&#255;');
+        StringText=strrep(StringText,'','&#191;');
+        StringText=strrep(StringText,'','&#192;');
+        StringText=strrep(StringText,'','&#193;');
+        StringText=strrep(StringText,'','&#194;');
+        StringText=strrep(StringText,'','&#195;');
+        StringText=strrep(StringText,'','&#196;');
+        StringText=strrep(StringText,'','&#197;');
+        StringText=strrep(StringText,'','&#198;');
+        StringText=strrep(StringText,'','&#199;');
+        StringText=strrep(StringText,'','&#200;');
+        StringText=strrep(StringText,'','&#201;');
+        StringText=strrep(StringText,'','&#202;');
+        StringText=strrep(StringText,'','&#203;');
+        StringText=strrep(StringText,'','&#204;');
+        StringText=strrep(StringText,'','&#205;');
+        StringText=strrep(StringText,'','&#206;');
+        StringText=strrep(StringText,'','&#207;');
+        StringText=strrep(StringText,'','&#208;');
+        StringText=strrep(StringText,'','&#209;');
+        StringText=strrep(StringText,'','&#210;');
+        StringText=strrep(StringText,'','&#211;');
+        StringText=strrep(StringText,'','&#212;');
+        StringText=strrep(StringText,'','&#213;');
+        StringText=strrep(StringText,'','&#214;');
+        StringText=strrep(StringText,'','&#215;');
+        StringText=strrep(StringText,'','&#216;');
+        StringText=strrep(StringText,'','&#217;');
+        StringText=strrep(StringText,'','&#218;');
+        StringText=strrep(StringText,'','&#219;');
+        StringText=strrep(StringText,'','&#220;');
+        StringText=strrep(StringText,'','&#221;');
+        StringText=strrep(StringText,'','&#222;');
+        StringText=strrep(StringText,'','&#223;');
+        StringText=strrep(StringText,'','&#224;');
+        StringText=strrep(StringText,'','&#225;');
+        StringText=strrep(StringText,'','&#226;');
+        StringText=strrep(StringText,'','&#227;');
+        StringText=strrep(StringText,'','&#228;');
+        StringText=strrep(StringText,'','&#229;');
+        StringText=strrep(StringText,'','&#230;');
+        StringText=strrep(StringText,'','&#231;');
+        StringText=strrep(StringText,'','&#232;');
+        StringText=strrep(StringText,'','&#233;');
+        StringText=strrep(StringText,'','&#234;');
+        StringText=strrep(StringText,'','&#235;');
+        StringText=strrep(StringText,'','&#236;');
+        StringText=strrep(StringText,'','&#237;');
+        StringText=strrep(StringText,'','&#238;');
+        StringText=strrep(StringText,'','&#239;');
+        StringText=strrep(StringText,'','&#240;');
+        StringText=strrep(StringText,'','&#241;');
+        StringText=strrep(StringText,'','&#242;');
+        StringText=strrep(StringText,'','&#243;');
+        StringText=strrep(StringText,'','&#244;');
+        StringText=strrep(StringText,'','&#245;');
+        StringText=strrep(StringText,'','&#246;');
+        StringText=strrep(StringText,'','&#247;');
+        StringText=strrep(StringText,'','&#248;');
+        StringText=strrep(StringText,'','&#249;');
+        StringText=strrep(StringText,'','&#250;');
+        StringText=strrep(StringText,'','&#251;');
+        StringText=strrep(StringText,'','&#252;');
+        StringText=strrep(StringText,'','&#253;');
+        StringText=strrep(StringText,'','&#254;');
+        StringText=strrep(StringText,'','&#255;');
     end
     StringText=deblank(StringText);
 end
